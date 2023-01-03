689e10ef8bad105f12dfd9e6b8477cf384bfa474
I.Chandana <indu.99.ic@gmail.com>
2023-01-03 06:33:05 +0530

    Jan03-001

--- client/script.js
+++ client/script.js
@@ -17,3 +17,50 @@ function loarder(element) {
     }
   }, 300) // 300 millisec
 }
+
+function typeText(element, text) {
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if (index < text.length) {  // we are still typing.
+      element.innerHTML += text.chartAt(index) // get character under a specific index in the text that AI is going to return
+      index++;
+    } else {
+      clearInterval(interval)
+    }
+  }, 20) // 20 millisec
+}
+
+function generateUniqueId() {
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timestamp}-${hexadecimalString}`
+}
+
+function chatStripe(isAi, value, uniqueId) {
+  return (
+    `
+    <div class="wrapper ${isAi && 'ai'}">
+      <div class="chat">
+        <div class="profile">
+            <img
+            src="${isAi ? bot : user}"
+            />
+        </div>
+        <div class="message" id=${uniqueId}>${value}</div>
+      </div>
+    </div>
+    `
+  )
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+   const data = new FormData(form);
+
+   // user's chatStripe
+   chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+   form.reset();
+}
